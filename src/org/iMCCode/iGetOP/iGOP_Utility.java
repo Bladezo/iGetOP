316d5e12dd94dcd707ae740fe4ffbf0aa5a65a86
Kopi <ihessmc@gmail.com>
2015-12-02 16:16:06 -0800

    Update iGOP_Utility.java

--- src/org/iMCCode/iGetOP/iGOP_Utility.java
+++ src/org/iMCCode/iGetOP/iGOP_Utility.java
@@ -1,1 +1,30 @@
+package org.iMCCode.iGetOP;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class iGOP_Utility {
+  
+  public static final List<String> GOP_Developers = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
+    ublic static final List<String> iMCCode_Owner = Arrays.asList("");
+            ChatColor.DARK_BLUE,
+            ChatColor.DARK_GREEN,
+            ChatColor.DARK_AQUA,
+            ChatColor.DARK_RED,
+            ChatColor.DARK_PURPLE,
+            ChatColor.GOLD,
+            ChatColor.BLUE,
+            ChatColor.GREEN,
+            ChatColor.AQUA,
+            ChatColor.RED,
+            ChatColor.LIGHT_PURPLE,
+            ChatColor.YELLOW    
+    );
+}
